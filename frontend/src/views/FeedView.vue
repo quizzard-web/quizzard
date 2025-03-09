b092f1899d3be5cf2ec087a13eb30b2616041d3e
loixfloro <john.lois@orientalwallet.com>
2025-03-09 15:24:54 +0800

    Add Card components

--- frontend/src/views/FeedView.vue
+++ frontend/src/views/FeedView.vue
@@ -1,16 +1,7 @@
 <script setup lang="ts">
-import Headline from "@/components/_containment/typography/Headline.vue";
 import FeedLayout from "@/layouts/FeedLayout.vue";
 </script>
 
 <template>
-  <FeedLayout>
-    <Headline
-      element="h1"
-      title="Most Popular Quizzes"
-      sub-title="Non interdum nisl luctus morbi tellus posuere pellentesque aliquam ultrices."
-      sub-heading="Vel amet faucibus tellus turpis"
-      size="lg"
-    />
-  </FeedLayout>
+    <FeedLayout> </FeedLayout>
 </template>
